25ac1758ebd1e29a611bba33d79ac34ccf4baf3c
RobertGotzen <contact@robert-gotzen.de>
2026-01-15 19:54:52 +0100

    # Adding app.storyblok.com to CORS headers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://app.storyblok.com',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
